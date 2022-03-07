63a045d2702c11ff8b9058f8533cac4650edadaf
Nikaizer <fernandezdanielalejandro93@gmail.com>
2022-03-07 12:11:26 -0300

    primer commit

---
+++ catalago/catalago/settings/production.py
@@ -0,0 +1,11 @@
+DEBUG = False
+
+ALLOWED_HOSTS = ['midominio.com.ar']
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    #aca va la base dato que va a correr en internet
+    }
+}
